ca962cba086567a25304528da920dcba80546458
Diana Badas <diana.badas@gmail.com>
2019-05-29 17:17:34 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
-
+  #Title must not be blank and repeated
   validates :title, presence: true, uniqueness: true
   validates_presence_of :release_year, :if => :released?
   validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, :if => :released?
